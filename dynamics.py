Added euler lagrange formulation to dynamics, also added some changes and fixes to kinematics, e.g. jacobdot, jacobian shifting, etc.

dynamics.py
@@ -10,7 +10,9 @@ https://github.com/Tarnarmour/RoboPy.git
 """
 
 import numpy as np
-from .kinematics import SerialArm
+from .kinematics import SerialArm, shift_gamma
+from .transforms import transl, se3
+from .utility import skew
 
 eye = np.eye(4)
 
@@ -25,13 +27,13 @@ class SerialArmDyn(SerialArm):
                  r_com=None,
                  link_inertia=None,
                  motor_inertia=None):
-        kin.SerialArm.__init__(self, dh, jt, base, tip, joint_limits)
+        SerialArm.__init__(self, dh, jt, base, tip, joint_limits)
         self.mass = mass
         self.r_com = r_com
         self.link_inertia = link_inertia
         self.motor_inertia = motor_inertia
 
-    def RNE(self, q, qd, qdd,
+    def rne(self, q, qd, qdd,
             Wext=np.zeros((6,)),
             g=np.zeros((3,)),
             omega_base=np.zeros((3,)),
@@ -121,3 +123,114 @@ class SerialArmDyn(SerialArm):
 
         return tau, Wrench
 
+    def jacobdot_com(self, q, qd, index, base=False, tip=False):
+
+        if index is None:
+            index = self.n
+        elif index > self.n:
+            print("WARNING: Index greater than number of joints!")
+            print(f"Index: {index}")
+            return None
+        elif index < 1:
+            print("WARNING: Index for Jacobdot cannot be less than 1!")
+            print(f"Index: {index}")
+            return None
+
+        Jdot = np.zeros((6, self.n))
+        Ae = self.fk(q, index) @ transl(self.r_com[index-1])
+        re = Ae[0:3, 0:3] @ self.r_com[index-1]
+        pe = Ae[0:3, 3]
+        xde = shift_gamma(re) @ self.jacob(q, index) @ qd
+        ve = xde[0:3]
+
+        for i in range(index):
+            Ac = self.fk(q, i)
+            pc = Ac[0:3, 3]
+            zc = Ac[0:3, 2]
+            xdc = self.jacob(q, i) @ qd
+            vc = xdc[0:3]
+            wc = xdc[3:6]
+            Jdot[0:3, i] = np.cross(zc, ve - vc) + np.cross(np.cross(wc, zc), pe - pc)
+            Jdot[3:6, i] = np.cross(wc, zc)
+
+        return Jdot
+
+    def get_M(self, q):
+        M = np.zeros((self.n, self.n))
+
+        for i in range(self.n):
+            A = self.fk(q, i+1)
+            R = A[0:3, 0:3]
+            ri = R @ self.r_com[i]
+            J = shift_gamma(ri) @ self.jacob(q, i + 1)
+            Jv = J[0:3, :]
+            Jw = J[3:6, :]
+            M += self.mass[i] * Jv.T @ Jv + Jw.T @ R @ self.link_inertia[i] @ R.T @ Jw
+
+        return M
+
+    def get_C(self, q, qd, Mdot=False):
+
+        C = np.zeros((self.n, self.n))
+        Mdot = np.zeros((self.n, self.n))
+        for i in range(self.n):
+            Ac = self.fk(q, i+1) @ transl(self.r_com[i])
+            R = Ac[0:3, 0:3]
+            rc = R @ self.r_com[i]
+            J = shift_gamma(rc) @ self.jacob(q, i+1)
+            Jv = J[0:3, :]
+            Jw = J[3:6, :]
+            Jd = self.jacobdot_com(q, qd, i+1)
+            Jvd = Jd[0:3, :]
+            Jwd = Jd[3:6, :]
+            Rd = skew(Jw @ qd) @ R
+            m = self.mass[i]
+            In = self.link_inertia[i]
+            C = C + m * Jv.T @ Jvd + Jw.T @ R @ In @ R.T @ Jwd + Jw.T @ Rd @ In @ R.T @ Jw
+
+            Ind = R @ In @ Rd.T + Rd @ In @ R.T
+            Mdot = Mdot + m * (Jv.T @ Jvd + Jvd.T @ Jv) + Jw.T @ (In @ Jwd + Ind @ Jw) + Jwd.T @ In @ Jw
+
+        if Mdot:
+            output = (C, Mdot)
+        else:
+            output = C
+        return output
+
+    def get_G(self, q, g=np.array([0, 0, -9.81])):
+
+        G = np.zeros((self.n,))
+        for i in range(self.n):
+            ri = self.fk(q, i+1)[0:3, 0:3] @ self.r_com[i]
+            J = shift_gamma(ri) @ self.jacob(q, i+1)
+            G = G - J.T @ np.hstack((g, np.zeros((3,))))
+
+        return G
+
+    def EL(self, q, qd, qdd, g=np.zeros([0, 0, 0]), Wext=np.zeros((6,))):
+
+        M = np.zeros((self.n, self.n))
+        C = np.zeros_like(M)
+        G = np.zeros((self.n,))
+
+        for i in range(self.n):
+            A = self.fk(q, i+1) @ transl(self.r_com[i])
+            R = A[0:3, 0:3]
+            r = R @ self.r_com[i]
+            J = shift_gamma(r) @ self.jacob(q, i + 1)
+            Jv = J[0:3, :]
+            Jw = J[3:6, :]
+            Jd = self.jacobdot_com(q, qd, i + 1)
+            Jvd = Jd[0:3, :]
+            Jwd = Jd[3:6, :]
+            Rd = skew(Jwd @ qd) @ R
+            m = self.mass[i]
+            In = self.link_inertia[i]
+
+            M = M + m * Jv.T @ Jv + Jw.T @ R @ In @ R.T @ Jw
+            C = C + m * Jv.T @ Jvd + Jw.T @ R @ In @ R.T @ Jwd + Jw.T @ Rd @ In @ R.T @ Jw
+            G = G - J.T @ np.hstack((g, np.zeros(3,)))
+
+        J = self.jacob(q)
+        tau = M @ qdd + C @ qd + G - J.T @ Wext
+        return tau

kinematics.py
@@ -188,6 +188,34 @@ class SerialArm:
 
         return J
 
+    def jacobdot(self, q, qd, index=None, base=False, tip=False):
+
+        if index is None:
+            index = self.n
+        elif index > self.n:
+            print("WARNING: Index greater than number of joints!")
+            print(f"Index: {index}")
+
+        Jdot = np.zeros((6, self.n), dtype=np.float32)
+        Te = self.fk(q, index, base=base, tip=tip)
+        pe = Te[0:3, 3]
+        Je = self.jacob(q, index, base, tip)
+        ve = Je[0:3, :] @ qd
+
+        for i in range(index):
+            if self.jt[i] == 'r':
+                T = self.fk(q, i, base=base, tip=tip)
+                z_axis = T[0:3, 2]
+                xd = self.jacob(q, i, base, tip) @ qd
+                Jdot[0:3, i] = np.cross(z_axis, ve - xd[0:3]) + np.cross(xd[3:6], Je[0:3, i])  # np.cross(np.cross(xd[3:6], z_axis), pe - pc)
+                Jdot[3:6, i] = np.cross(xd[3:6], z_axis)
+            else:
+                T = self.fk(q, i, base=base, tip=tip)
+                z_axis = T[0:3, 2]
+                Jdot[0:3, i] = np.cross(xd[3:6, z_axis])
+
+        return Jdot
+
     def jacoba(self, q, rep='rpy', index=None, eps=1e-4):
 
         if rep == 'rpy':
@@ -202,7 +230,8 @@ class SerialArm:
         elif rep == 'axis':
             def get_pose(q):
                 return A2axis(self.fk(q, index))
-        elif rep == 'q' or rep == 'quaternion':
+        elif rep == 'q' or rep == 'quaternion' or rep == 'quat':
+            return padE(invEquat(A2q(self.fk(q, index))[3:7])) @ self.jacob(q, index)
             def get_pose(q):
                 return A2q(self.fk(q, index))
         elif rep == 'x':
@@ -310,6 +339,19 @@ class SerialArm:
         output = IKOutput(q, xf, x_target, status, report, count, e, norm(e))
         return output
 
+    # def ik_pinv(self, At, fk, jacob):
+
+def shift_gamma(*args):
+    gamma = np.eye(6)
+    for x in args:
+        if len(x.shape) == 1:
+            # shifting by an offset
+            gamma_new = np.block([[np.eye(3), -skew(x)], [np.zeros((3,3)), np.eye(3)]])
+        else:
+            # rotation of jacobian
+            gamma_new = np.block([[x.T, np.zeros((3,3))], [np.zeros((3,3)), x.T]])
+        gamma = gamma @ gamma_new
+    return gamma
 
 class IKOutput:
     def __init__(self, qf, xf, xt, status, report, nit, ef, nef):
@@ -324,18 +366,4 @@ class IKOutput:
 
     def __str__(self):
         return f"IK OUTPUT:\nq final: {self.qf} \nStatus: {self.status} \nMessage: {self.message} \nIteration number: {self.nit} \nTarget Pose: {self.xt}\nFinal Pose: {self.xf}\nFinal Error: {self.ef} \nFinal Error Norm: {self.nef}"
-
-if __name__ == "__main__":
-    np.set_printoptions(precision=4, floatmode='maxprec', suppress=True)
-    pi = np.pi
-    dh = [[0, 0, 0.4, pi/2]] * 10
-    jt = ['r'] * 10
-    q0 = [0] * len(jt)
-    arm = SerialArm(dh, jt)
-    A_target = se3(rotz(np.pi/2), [1, 1, 1])
-
-    viz.addScatter(A_target[0:3, 3], np.array([0, 1, 0, 1]), 10)
-    output = arm.ik(A_target, q0, 'pinv', 'rpy', 300)
-
-    print(output)
 

scripts/animation_test.py
@@ -3,9 +3,10 @@ import numpy as np
 from numpy import pi, sin, cos, sqrt, cross
 from numpy.linalg import norm
 
-n = 9
+n = 7
 max_length = 0.5
 dh = np.random.random_sample((n,4)) * 2 * max_length - max_length
+dh = [[pi/6, 0, 0.25, pi/3]] * n
 q = np.zeros((n,))
 jt = ['r'] * len(dh)
 tip = se3()
@@ -16,8 +17,7 @@ viz.add_arm(arm, draw_frames=False)
 
 viz.update()
 
-A_target = arm.fk(np.random.random_sample((n,)))
-viz.add_frame(A_target)
+
 viz.update(q)
 
 max_speed = 0.05
@@ -29,15 +29,37 @@ print(dq)
 #     q = q + dq
 #     viz.update(q)
 
-sol = arm.ik(A_target,
-            method='pinv',
-            rep='rpy',
-            max_iter=np.inf,
-            tol=1e-3,
-            viz=viz,
-            min_delta=1e-5,
-            max_delta=10)
+count_q = 0
+count_rpy = 0
 
-print(sol)
+for i in range(10):
+    A_target = arm.fk(np.random.random_sample((n,)))
+    viz.add_frame(A_target)
+    sol = arm.ik(A_target,
+                 q0=q,
+                method='pinv',
+                rep='cart',
+                max_iter=np.inf,
+                tol=1e-3,
+                viz=viz,
+                min_delta=1e-5,
+                max_delta=0.01)
+    count_q += sol.status
+    q = sol.qf
+
+    viz.remove_frame()
+
+    # sol = arm.ik(A_target,
+    #              method='jt',
+    #              rep='cart',
+    #              max_iter=50,
+    #              tol=1e-3,
+    #              viz=viz,
+    #              min_delta=1e-5,
+    #              max_delta=0.1)
+    # count_rpy += sol.status
+
+print(count_q)
+print(count_rpy)
 
 viz.hold()

transforms.py
@@ -116,10 +116,10 @@ def euler2R(th1, th2, th3, order='xyz'):
     return clean_rotation_matrix(R)
 
 def R2q(R):
-    return np.array([0.5*sqrt(R[0, 0] + R[1, 1] + R[2, 2] + 1),
-                     0.5*np.sign(R[2, 1] - R[1, 2]) * sqrt(R[0, 0] - R[1, 1] - R[2, 2] + 1),
-                     0.5*np.sign(R[0, 2] - R[2, 0]) * sqrt(R[1, 1] - R[2, 2] - R[0, 0] + 1),
-                     0.5*np.sign(R[2, 1] - R[1, 2]) * sqrt(R[2, 2] - R[0, 0] - R[1, 1] + 1)])
+    return np.array([0.5*sqrt(np.abs(R[0, 0] + R[1, 1] + R[2, 2] + 1)),
+                     0.5*np.sign(R[2, 1] - R[1, 2]) * sqrt(np.abs(R[0, 0] - R[1, 1] - R[2, 2] + 1)),
+                     0.5*np.sign(R[0, 2] - R[2, 0]) * sqrt(np.abs(R[1, 1] - R[2, 2] - R[0, 0] + 1)),
+                     0.5*np.sign(R[2, 1] - R[1, 2]) * sqrt(np.abs(R[2, 2] - R[0, 0] - R[1, 1] + 1))])
 
 def q2R(q):
     nu = q[0]
@@ -195,3 +195,65 @@ def A2q(A):
 def A2x(A):
     return np.hstack((A[0:3, 3], np.reshape(A[np.ix_([0,1,2], [0,2])], 6, 'F')))
 
+def A2cart(A):
+    return A[0:3, 3]
+
+def A2pose(A, pose='q'):
+    def f(x):
+        return {
+            'rpy': A2rpy(A),
+            'planar': A2planar(A),
+            'axis': A2axis(A),
+            'q': A2q(A),
+            'quat': A2q(A),
+            'x': A2x(A),
+            'cart': A2cart(A)
+        }.get(pose, A2q(A))
+    return f(pose)
+
+def Erpy(x):
+    spsi = sin(x[0])
+    cpsi = cos(x[0])
+    sth = sin(x[1])
+    cth = cos(x[1])
+    E = np.array([[0, -spsi, cth*cpsi],
+                  [0, cpsi, cth*spsi],
+                  [1, 0, -sth]])
+    return E
+
+def invErpy(x):
+    spsi = sin(x[0])
+    cpsi = cos(x[0])
+    sth = sin(x[1])
+    cth = cos(x[1])
+    if np.abs(np.abs(np.mod(x[1], np.pi)) - np.pi) < 1e-6:
+        E = np.array([[0, 0, 0.5],
+                      [-spsi, cpsi, 0],
+                      [0, 0, 0.5]])
+    else:
+        E = np.array([[cpsi*sth/cth, sth*spsi/cth, 1],
+                      [-spsi, cpsi, 0],
+                      [cpsi/cth, spsi/cth, 0]])
+    return E
+
+def Eaxis(x):
+    E = np.hstack((x[1:4,None], sin(x[0])*np.eye(3) + (1-cos(x[0]))*skew(x[1:4])))
+    return E
+
+def invEaxis(x):
+    E = np.vstack((x[1:4], -sin(x[0]) / 2 / (1 - cos(x[0])) * skew(x[1:4])**2 - skew(x[1:4]) / 2))
+    return E
+
+def Equat(x):
+    H = np.hstack((-x[1:4, None], skew(x[1:4]) + x[0]*np.eye(3)))
+    return 2 * H
+
+def invEquat(x):
+    H = np.hstack((-x[1:4, None], skew(x[1:4]) + x[0] * np.eye(3)))
+    return H.T / 2
+
+def padE(E):
+    m = E.shape[0]
+    n = E.shape[1]
+    return np.block([[np.eye(3), np.zeros((3,n))],
+                     [np.zeros((m,3)), E]])